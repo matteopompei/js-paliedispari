87e40f37cd433575301a2ad084422083877afd96
Matteo Pompei <datxizzan@gmail.com>
2021-12-17 17:16:36 +0100

    Parole palindrome rionosciute

--- Palidroma/script.js
+++ Palidroma/script.js
@@ -1,2 +1,22 @@
 // Chiedere all’utente di inserire una parola
 // Creare una funzione per capire se la parola inserita è palindroma
+
+let word = prompt("Inserisci una parola");
+let pal = "";
+
+// Controlliamo se la parola è un palindromo
+let lettersArr = word.split("");
+let reverseArray = lettersArr.reverse();
+let reverseWord = reverseArray.join("");
+if (word.toLowerCase() == reverseWord.toLowerCase()) {
+  pal = "La parola è palindroma!";
+}
+else {
+  pal = "La parola <strong>NON</strong> è palindroma!";
+}
+
+document.writeln("Parola inserita: " + word + "<br>");
+
+document.writeln("Parola invertita: " + reverseWord + "<br>");
+
+document.writeln(pal);
