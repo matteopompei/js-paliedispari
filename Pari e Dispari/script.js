289db144caaf711d73db13350684c9fb87c07d6c
Matteo Pompei <datxizzan@gmail.com>
2021-12-17 23:14:21 +0100

    IF aggiunto per accettare solo input PARI o DISP

--- Pari e Dispari/script.js
+++ Pari e Dispari/script.js
@@ -7,50 +7,57 @@
 
 // Chiedo di scegliere pari o dispari e poi un numero da 1 a 5
 let PD = prompt("PARI o DISPARI?");
-let nUser = parseInt(prompt("Scegli un numero da 1 a 5!"));
 
-// Controllo se sono stati inseriti dei numeri nel rage richiesto
-if (nUser > 5 || nUser < 1) {
-  do {
-    alert("Hai inserito un numero non valido!");
-    nUser = parseInt(prompt("Scegli un numero da 1 a 5!"));
+// Controlla se sa scrivere
+if (PD.toLowerCase() == "pari" || PD.toLowerCase() == "dispari") {
+  let nUser = parseInt(prompt("Scegli un numero da 1 a 5!"));
+
+  // Controllo se sono stati inseriti dei numeri nel rage richiesto
+  if ((nUser > 5) || (nUser < 1)) {
+    do {
+      alert("Hai inserito un numero non valido!");
+      nUser = parseInt(prompt("Scegli un numero da 1 a 5!"));
+    }
+    while ((nUser > 5) || (nUser < 1));
   }
-  while (nUser > 5 || nUser < 1);
-}
 
+  // Generiamo un numero random da 1 a 5 per il computer
+  // E inseriamolo in una funzione
+  function nRandom() {
+    return parseInt(Math.floor(Math.random() * 5) +1);
+  }
+  let nCPU = nRandom(); // Registriamo in memoria il numero assegnandolo ad una variabile
 
-// Generiamo un numero random da 1 a 5 per il computer
-// E inseriamolo in una funzione
-function nRandom() {
-  return parseInt(Math.floor(Math.random() * 5) +1);
-}
-let nCPU = nRandom(); // Registriamo in memoria il numero assegnandolo ad una variabile
+  sum = nUser + nCPU; // Sommiamo le due variabili con i numeri
 
-sum = nUser + nCPU; // Sommiamo le due variabili con i numeri
+  document.writeln("Hai scelto <strong>" + PD.toLowerCase() + "</strong><br>");
+  document.writeln("Il tuo numero: <strong>" + nUser + "</strong><br>");
+  document.writeln("Il numero del computer: <strong>" + nCPU + "</strong><br>");
+  document.writeln("La somma è <strong>" + sum + "</strong>, ");
 
-document.writeln("Hai scelto <strong>" + PD.toLowerCase() + "</strong><br>");
-document.writeln("Il tuo numero: <strong>" + nUser + "</strong><br>");
-document.writeln("Il numero del computer: <strong>" + nCPU + "</strong><br>");
-document.writeln("La somma è <strong>" + sum + "</strong>, ");
+  // Controlliamo se la somma delle due variabili è un numero pari o un numero dispari
+  function pdCheck() {
+    if (sum % 2 != 0) {
+      sum = "dispari";
+    }
+    else {
+      sum = "pari";
+    }
+    return sum;
+  }
 
-// Controlliamo se la somma delle due variabili è un numero pari o un numero dispari
-function pdCheck() {
-  if (sum % 2 != 0) {
-    sum = "dispari";
+  let sumCheck = pdCheck(); // Richiamiamo la funzione e assegnamola ad una variabile per tenerla in memoria
+  document.writeln("è un numero <strong>" + sumCheck + "</strong><br>");
+
+  // Dichiariamo chi ha vinto
+  if (PD.toLowerCase() == sumCheck) {
+    document.writeln("<div class=\"win\">Hai vinto, sei un campione!</div>");
   }
   else {
-    sum = "pari";
+    document.writeln("<div class=\"lose\">Il computer ha vinto, sei un perdente!</div>");
   }
-  return sum;
-}
-
-let sumCheck = pdCheck(); // Richiamiamo la funzione e assegnamola ad una variabile per tenerla in memoria
-document.writeln("è un numero <strong>" + sumCheck + "</strong><br>");
-
-// Dichiariamo chi ha vinto
-if (PD.toLowerCase() == sumCheck) {
-  document.writeln("<div class=\"win\">Hai vinto, sei un campione!</div>");
 }
 else {
-  document.writeln("<div class=\"lose\">Il computer ha vinto, sei un perdente!</div>");
+  alert("Impara a scrivere pagliaccio!");
+  location.reload();
 }
